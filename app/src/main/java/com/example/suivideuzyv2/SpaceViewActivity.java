3c5815d083d230d107bb8429a329db06311cf0d8
Theo Gaillard <theogllrd@gmail.com>
2020-02-02 22:37:19 +0100

    calendar setup | part1

--- app/src/main/java/com/example/suivideuzyv2/SpaceViewActivity.java
+++ app/src/main/java/com/example/suivideuzyv2/SpaceViewActivity.java
@@ -2,7 +2,9 @@ package com.example.suivideuzyv2;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,14 +12,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class SpaceViewActivity extends AppCompatActivity {
+public class SpaceViewActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,37 @@ public class SpaceViewActivity extends AppCompatActivity {
         Bundle bdl = this.getIntent().getExtras();
         String SpaceName = bdl.getString("SpaceName");
 
+
+        // --------------------------------------------
         // affichage de la date
+        //DialogFragment datePicker = new DatePickerFragment();
+        //Date date = new DatePickerFragment();
         Date currentTime = Calendar.getInstance().getTime();
-        this.setTitle(SpaceName + " - " + DateFormat.getDateInstance().format(currentTime));
+        this.setTitle(SpaceName + " - " + currentTime);
+
+
+
+
+
+
 
 
         this.initButtonIndicators();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText(currentDateString);
     }
 
     // Creation et affichage du menu
@@ -54,7 +83,9 @@ public class SpaceViewActivity extends AppCompatActivity {
                 break;
 
             case R.id.action_calendar :
-
+                //startActivity(new Intent(this,CalendarActivity.class));
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
                 break;
         }
         return super.onOptionsItemSelected(item);
