66b3050f7e5629dfc590d69110d39dd70a67ffa4
Theo Gaillard <theogllrd@gmail.com>
2020-02-02 11:04:00 +0100

    add calendar to menu

--- app/src/main/java/com/example/suivideuzyv2/SpaceViewActivity.java
+++ app/src/main/java/com/example/suivideuzyv2/SpaceViewActivity.java
@@ -1,13 +1,21 @@
 package com.example.suivideuzyv2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SpaceViewActivity extends AppCompatActivity {
 
@@ -18,13 +26,39 @@ public class SpaceViewActivity extends AppCompatActivity {
 
         Bundle bdl = this.getIntent().getExtras();
         String SpaceName = bdl.getString("SpaceName");
-        this.setTitle(SpaceName);
+
+        // affichage de la date
+        Date currentTime = Calendar.getInstance().getTime();
+        this.setTitle(SpaceName + " - " + DateFormat.getDateInstance().format(currentTime));
 
 
         this.initButtonIndicators();
     }
 
+    // Creation et affichage du menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_with_calendar, menu);
+        return true;
+    }
 
+
+    // gestion des clics sur les éléments du menu :
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_home :
+                // redirection vers l'activité home
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+
+            case R.id.action_calendar :
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
     // init des buttons
